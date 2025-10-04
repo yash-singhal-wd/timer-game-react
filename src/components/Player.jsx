2893ead9684c3be3237c1c2550fb609d8bacc1ef
ZafarWD <yash98singhal@gmail.com>
2025-10-05 00:12:19 +0530

    refining player.jsx

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
+
 export default function Player() {
+  //state related stuff
+  const [enteredName, setEnteredName] = useState("");
+  function handleNameChange(e){
+    setEnteredName(e.target.value);
+  }
+  //derived state stuff
+
+  //conditional content
+  const playerName = enteredName ? enteredName : "unknown entity";
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {playerName}</h2>
       <p>
-        <input type="text" />
+        <input value={enteredName} type="text" onChange={handleNameChange}/>
         <button>Set Name</button>
       </p>
     </section>
