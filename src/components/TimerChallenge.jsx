6babd07f55e670c676610a08c9b63b2e33a7edc0
ZafarWD <yash98singhal@gmail.com>
2025-10-05 22:52:36 +0530

    refactored logic - 2 state solution

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -2,25 +2,29 @@ import {useState} from 'react';
 
 export default function TimerChallenge({title, targetTime}) {
     //state related stuff
+    const [timerStarted, setTimerStarted] = useState(false);
     const [timerExpired, setTimerExpired] = useState(false);
     //derived state stuff
     function handleStart(){
+        setTimerStarted(true);
         setTimeout( () => {
             setTimerExpired(true);
         }, targetTime*1000);
     }
     //conditional components
-    const startLabel = timerExpired ? "Stop Challenge" : "Start Challenge";
-    const timerLabel = timerExpired ? "Timer is Running..." : undefined;
+    const startLabel = timerStarted ? "Stop Challenge" : "Start Challenge";
+    const timerLabel = timerStarted ?  "Timer is Running..." : undefined;
+    const timerClass = timerStarted ? "active" : undefined;
     return (
         <section className="challenge">
             <h2>{title}</h2>
             {timerExpired && <p>You fuckin lost</p>}
             <p className="challenge-time">
-                {targetTime} second(s)
+                {targetTime} second{`${targetTime > 1 ? 's' : ''}`}
             </p>
             <p><button onClick={handleStart}>{startLabel}</button></p>
-            <p className="active">{timerLabel}</p>
+            <p classname={timerClass}>{timerLabel}</p>
+
         </section>
     );
 }
